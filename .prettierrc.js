b15dbd862156747f1d1270242daf15a2b3c703b5
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-12-27 21:34:22 +0900

    setup project

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always"
+}
